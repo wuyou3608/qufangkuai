63cc5e2f8bcadfff7e2e30d46f6a6929548f8af4
wuyou3608 <37480166+wuyou3608@users.noreply.github.com>
2026-05-16 08:29:28 +0800

    Update no_cache_server.py

--- jimu/no_cache_server.py
+++ jimu/no_cache_server.py
@@ -1,16 +1,1 @@
-import http.server
-import socketserver
 
-class NoCacheRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        # 禁用缓存
-        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-        self.send_header("Pragma", "no-cache")
-        self.send_header("Expires", "0")
-        return super().end_headers()
-
-PORT = 8000
-
-with socketserver.TCPServer(("", PORT), NoCacheRequestHandler) as httpd:
-    print(f"Serving no-cache HTTP on port {PORT}")
-    httpd.serve_forever()
